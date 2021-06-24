b1bf97c30c9d6c334131832555aa98dfebac7bbf
Michal Ajziuk <michal.ajziuk@lingarogroup.com>
2021-06-24 17:42:09 +0200

    First review

---
+++ src/components/DashboardClient/pages/Progress/ProgressElements.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    background-color: #ccc;
+    padding-top: 10%;
+    margin-left: 5px;
+    padding-left: 5px;
+`
+
+export const RecentDateWrap = styled.div`
+    height: 30px;
+    width: 150px;
+    border: 2px solid;
+    
+`
+export const RecentDate = styled.h3`
+    font-size: 16px;
+    
+`
+export const WelcomeBlock = styled.div`
+`
+
+export const MotivationText = styled.div`
+`
+export const MotivationImage = styled.div`
+`
+
+export const Img = styled.div`
+`
+
+export const ChartBlock = styled.div`
+`
+
+export const SecondMotivationBlock = styled.div`
+`
+
+export const SecondMotivationText = styled.div`
+`
+
+export const BlogWrap = styled.div`
+`
+
+export const TitleArea = styled.div`
+`
+
+export const BlogPostCard = styled.div`
+`
+
+export const PostImage = styled.div`
+`
+
+export const PostSection = styled.div`
+`
+export const PostTitle = styled.div`
+`
+export const PostText = styled.div`
+`
+
+
+
